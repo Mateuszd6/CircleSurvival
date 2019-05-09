389667239654dd03c5c8fbb4a7f84f13f01c55fe
Mateusz Dudzinski <mateuszpd7@gmail.com>
2019-05-09 19:51:36 +0200

    Some work with spawning and destructing circles

--- Assets/Circle.cs
+++ Assets/Circle.cs
@@ -1,52 +1,105 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
-public class Circle : MonoBehaviour
+public class Circle : MonoBehaviour, IPointerClickHandler
 {
     public Image greenFill;
     public RectTransform gameCanvasTransform;
 
+    public int id;
+    public float size = 0.0f;
     public float currentTime = 0.0f;
+    public float initTime = 0.2f;
     public float expireTime = 4.0f;
+
+    // When (de)initializing circle size lerps from(to) zero but the object is inactive. 
+    // This make is 'smoothely' appear on the screen.
+    public enum CircleState { initializing, couting, deinitializing };
+    public CircleState circleState = CircleState.initializing;
     
     Vector2 RandomScreenPosition(float circleSize)
     {
         var screenSize = new Vector2(gameCanvasTransform.rect.width, 
                                      gameCanvasTransform.rect.height);
 
-        var randomPos = new Vector2(Random.Range(circleSize, screenSize.x), 
-                                    Random.Range(circleSize, screenSize.y));
+        // This offset is added to the position, so that the circle 
+        // will never be right on the boundary.
+        float offset = 5;
+        float circleRadius = circleSize / 2;
+        var randomPos = new Vector2(Random.Range(circleRadius + offset, screenSize.x - circleRadius - offset), 
+                                    Random.Range(circleRadius + offset, screenSize.y - circleRadius - offset));
 
         return randomPos;
     }
 
     void Start()
     {
+        id = Random.Range(0, 2147483647);
         var selfRectTransfrom = GetComponent<RectTransform>();
 
         float w = gameCanvasTransform.rect.width;
         float h = gameCanvasTransform.rect.height;
         float circlew = selfRectTransfrom.rect.width;
         float circleh = selfRectTransfrom.rect.height;
-        selfRectTransfrom.sizeDelta = new Vector2(120, 120);
 
-        Vector2 randomPosition = RandomScreenPosition(120);
+
+        var gameSettings = Singles.GetScriptable<GameSettings>();
+        size = Random.Range(gameSettings.circleSizeMin, gameSettings.circleSizeMax);
+        selfRectTransfrom.sizeDelta = new Vector2(size, size);
+        Vector2 randomPosition = RandomScreenPosition(size);
         selfRectTransfrom.position = new Vector3(randomPosition.x, randomPosition.y);
         Debug.Log("Size: (" + w + "; " + h + "), Circle: (" + circlew + "; " + circleh + ")" + "\n");
     }
 
     void Update()
     {
-        if (greenFill.fillAmount > 0)
+        currentTime += Time.deltaTime;
+
+        if (circleState == CircleState.initializing)
         {
-            currentTime += Time.deltaTime;
+            var selfRectTransfrom = GetComponent<RectTransform>();
+            float newScale = Mathf.Lerp(0.0f, size, currentTime / 0.08f);
+            selfRectTransfrom.sizeDelta = new Vector2(newScale, newScale);
+
+            if (newScale >= size)
+            {
+                currentTime = 0;
+                circleState = CircleState.couting;
+            }
+        }
+        
+        if (circleState == CircleState.couting)
+        {
             greenFill.fillAmount = Mathf.Lerp(1, 0, currentTime / expireTime);
+
+            if (greenFill.fillAmount <= Mathf.Epsilon)
+            {
+                Debug.Log("Not clicked! Game should be finished!\n");
+                Debug.Break();
+
+                // TODO: We won't need these.
+                currentTime = 0;
+                circleState = CircleState.deinitializing;
+            }
         }
 
-        if (currentTime > 4)
+        if (circleState == CircleState.deinitializing)
         {
-            currentTime = 0;
-            greenFill.fillAmount = 1;
+            var selfRectTransfrom = GetComponent<RectTransform>();
+            float newScale = Mathf.Lerp(size, 0.0f, currentTime / 0.08f);
+            selfRectTransfrom.sizeDelta = new Vector2(newScale, newScale);
+
+            if (newScale <= Mathf.Epsilon)
+                Destroy(gameObject);
         }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Circle " + id + " was clicked!\n");
+        circleState = CircleState.deinitializing;
+        currentTime = 0;
     }
 }
---
+++ Assets/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private List<Circle> activeCircles;
+
+    void Awake()
+    {
+        activeCircles = new List<Circle>();
+    }
+
+    public void SpwanCircle()
+    {
+        ;        
+    }
+
+    // 
+    public void DestroyCircle(int circleID)
+    {
+        activeCircles.RemoveAll(x => x.id == circleID);
+    }
+}
